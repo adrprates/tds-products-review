bc4ee4145b06c4b603844fa02d3a828bb319dad2
Adriano Prates <adrianopratessouza@gmail.com>
2024-09-23 15:38:01 -0300

    Projeto inicial

---
+++ src/main/java/com/senacead/productsreview/ProductsreviewApplication.java
@@ -0,0 +1,13 @@
+package com.senacead.productsreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senacead/productsreview/ProductsreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senacead.productsreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
